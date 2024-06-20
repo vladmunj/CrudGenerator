f7503763f9152c3796db10c85f185088ea7a3040
vladmunj <mun.vladislav.a@gmail.com>
2024-06-20 15:49:27 +0500

    undo

--- src/Console/CrudRefreshCommand.php
+++ src/Console/CrudRefreshCommand.php
@@ -40,7 +40,6 @@ class CrudRefreshCommand extends Command{
      */
     public function handle()
     {
-        $this->migrate(); // validate migrations is correct before drop tables and data
         $tablesData = $this->loadTablesData();
         $foreignKeysInfo = [];
         foreach($tablesData as $table){
